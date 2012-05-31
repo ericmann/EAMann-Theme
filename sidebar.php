3bd18e85d72250aa41dbac8abf548cdcf62024cd
Eric Mann <eric@eamann.com>
2012-05-31 07:22:10 -0700

    Sidebar styling
    Add a background and some color for the sidebar. Move the page header to the top of the sidebar where it belongs.

--- sidebar.php
+++ sidebar.php
@@ -8,6 +8,9 @@
 ?>
 		<div id="secondary" class="widget-area" role="complementary">
 			<?php do_action( 'before_sidebar' ); ?>
+			<aside id="site-header">
+				<h1 class="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			</aside>
 			<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
 
 				<aside id="search" class="widget widget_search">
